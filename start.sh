411294cd2f86078793f1ee58a16d16bd2ca3e848
Eneji Victor <enejivic@gmail.com>
2025-08-22 23:37:04 +0100

    adding moving into a directory

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
-
 #!/bin/bash
-
+cd "$(dirname "$0")"   # go into the script's folder
 source .venv/bin/activate
-# Start FastAPI app with Uvicorn, log output, and run in background
 uvicorn main:app --host 0.0.0.0 --port 8000 --workers 9
